37b3c141c03cbd2a19d805f06dc33f64269ae641
vishu3278 <vishu3278@gmail.com>
2018-05-07 18:16:12 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var myapp = new Vue({
         posts: [],
         pages: [],
         errors: [],
+        loading:false,
         lat:'',
         long:'',
         model:'',
@@ -15,13 +16,19 @@ var myapp = new Vue({
     },
     methods: {
         getPost: function() {
+            this.loading = true;
             axios.get('https://wowitsolutions.com/antardirshti/wp-json/wp/v2/posts')
-                .then(response => {this.posts = response.data})
+                .then(response => {
+                    this.posts = response.data;
+                    this.loading = false})
                 .catch(e => {this.errors.push(e)});
         },
         getPages: function() {
+            this.loading = true,
             axios.get('https://wowitsolutions.com/antardirshti/wp-json/wp/v2/pages')
-                .then(response => {this.pages = response.data})
+                .then(response => {
+                    this.pages = response.data;
+                    this.loading = false})
                 .catch(e => {this.errors.push(e)});
         }
     }
