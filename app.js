cd09082059c3ad5ffaf023712f9cc66525993849
vishu3278 <vishu3278@gmail.com>
2018-05-10 19:43:53 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    // alert('Connection type: ' + states[networkState]);
+    if (networkState !== Connection.NONE) {
+        myapp.network = true;
+        navigator.vibrate([200, 400, 200]);
+    }else{
+        myapp.network = false;
+    }
+}
 
 var myapp = new Vue({
     el: "#app",
